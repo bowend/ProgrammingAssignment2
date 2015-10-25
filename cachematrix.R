8d78ab3e54ebde907c9e41badc665f2bb1204cd4
Dan Bowen <Daniel.J.Bowen@gmail.com>
2015-10-25 14:06:22 +0000

    Created matrix cache functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## theCacheMatrix function returns a list of functions allowing the user to set a Matrix and cache its inverse within the makeCacheMatrix environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    ## the set function takes a new matrix as an argument and resets the cached mean to NULL (in the makeChacheMatrix environment)
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    ## the get function returns the matrix x
+    get <- function() x
+    ## sets the cached mean object 'm' (in the makeCacheMatrix environment to its argument 
+    setinverse <- function(inverse) inv <<- inv
+    ## 
+    getinverse <- function() inv
+    list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Takes a cacheMatrix object (as returned by makeCacheMatrix) and if the matrix is square, returns its inverse cacheing the result to reuse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    ## checks whether the chached inverse is NULL (a NULL value is reset every time a new matrix is stored to avoid returning cached results for the wrong matrix)
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
